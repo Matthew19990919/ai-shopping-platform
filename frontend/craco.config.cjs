4fe7f5c5286b047270d287c589aef01b5af9a641
Matthew19990919 <504525675@qq.com>
2025-04-07 20:58:33 +0800

    重命名craco.config.js为craco.config.cjs以解决ES模块问题

---
+++ frontend/craco.config.cjs
@@ -0,0 +1,46 @@
+const CracoLessPlugin = require('craco-less');
+const path = require('path');
+const theme = require('./src/theme').default;
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: theme,
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@contexts': path.resolve(__dirname, 'src/contexts'),
+    },
+  },
+  style: {
+    postcss: {
+      plugins: [
+        require('postcss-flexbugs-fixes'),
+        require('postcss-preset-env')({
+          autoprefixer: {
+            flexbox: 'no-2009',
+          },
+          stage: 3,
+        }),
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+};
